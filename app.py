00a09bd81dff2555ca24991c9fbbf8ffca1f8fe5
Harry Sharma <harrysharma1066@gmail.com>
2025-02-22 13:13:38 +0000

    feat: add form for single and multiple cifar indexes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import base64
 from io import BytesIO
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 from torchvision import datasets
 
 app = Flask(__name__)
@@ -8,15 +8,27 @@ dst = datasets.CIFAR100("~/.torch", download=True)
 
 @app.route("/")
 def index():
-    images = []
-    for idx in range(25):
-        img = dst[idx][0]
-        buffer = BytesIO()
-        img.save(buffer, format="PNG")
-        img_str = base64.b64encode(buffer.getvalue()).decode()
-        images.append(img_str)
-        
-    return render_template("index.html", images = images)
+    return render_template("index.html")
+
+@app.route('/handle_data_single', methods=['POST'])
+def handle_data_single():
+    cifar_index = request.form['cifar_index']
+    activation_function = request.form['activation_function']
+    
+    print(f"CIFAR Index: {cifar_index}")
+    print(f"Activation Function: {activation_function}")
+    return redirect(url_for('index'))
+
+@app.route('/handle_data_multiple', methods=['POST'])
+def handle_data_multiple():
+    start_cifar_index = request.form['start_cifar_index']
+    end_cifar_index = request.form['end_cifar_index']
+    activation_function = request.form['activation_function']
+    
+    print(f"Start CIFAR Index: {start_cifar_index}")
+    print(f"End CIFAR Index: {end_cifar_index}")
+    print(f"Activation Function: {activation_function}")
+    return redirect(url_for('index'))
  
 if __name__ == '__main__':
 	app.run(debug=True)
